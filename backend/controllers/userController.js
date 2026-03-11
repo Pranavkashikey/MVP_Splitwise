80bc6a6075644e6f94aa8c6f8d96e1ec2a198ff0
Pranav Kashikey <pranavkashikey93@gmail.com>
2026-03-11 22:17:58 +0530

    User update , view and delete routes done

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -13,3 +13,63 @@ currency
 res.json(user)
 
 }
+
+exports.getUser = async (req,res)=>{
+
+const id = req.params.id
+
+try{
+
+const user = await User.findByPk(id)
+
+res.json(user)
+
+}catch(error){
+
+res.status(500).json({error:error.message})
+
+}
+
+}
+
+exports.updateUser = async (req,res)=>{
+
+const id = req.params.id
+const {email,currency} = req.body
+
+try{
+
+await User.update(
+{email,currency},
+{where:{id}}
+)
+
+res.json({message:"User updated successfully"})
+
+}catch(error){
+
+res.status(500).json({error:error.message})
+
+}
+
+}
+
+exports.deleteUser = async (req,res)=>{
+
+const id = req.params.id
+
+try{
+
+await User.destroy({
+where:{id}
+})
+
+res.json({message:"User deleted successfully"})
+
+}catch(error){
+
+res.status(500).json({error:error.message})
+
+}
+
+}
